d449f12a3f67b71002b5b14b1f64d5e155de0780
gounthar <gounthar@gmail.com>
2023-10-17 10:45:25 +0200

    Move to HTMLUnit 3.

--- src/test/java/org/jenkins_ci/plugins/flexible_publish/ConfigurationTest.java
+++ src/test/java/org/jenkins_ci/plugins/flexible_publish/ConfigurationTest.java
@@ -52,6 +52,10 @@ import hudson.tasks.junit.TestResultAction.Data;
 import hudson.tasks.junit.JUnitResultArchiver;
 import hudson.util.DescribableList;
 
+import org.htmlunit.html.HtmlCheckBoxInput;
+import org.htmlunit.html.HtmlForm;
+import org.htmlunit.html.HtmlInput;
+import org.htmlunit.html.HtmlPage;
 import org.jenkins_ci.plugins.flexible_publish.strategy.FailAtEndExecutionStrategy;
 import org.jenkins_ci.plugins.flexible_publish.strategy.FailFastExecutionStrategy;
 import org.jenkins_ci.plugins.run_condition.BuildStepRunner;
@@ -67,14 +71,10 @@ import org.jvnet.hudson.test.TestExtension;
 import org.jvnet.hudson.test.recipes.LocalData;
 import org.kohsuke.stapler.DataBoundConstructor;
 
-import com.gargoylesoftware.htmlunit.html.HtmlCheckBoxInput;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 
-import static com.gargoylesoftware.htmlunit.html.HtmlFormUtil.submit;
+import static org.htmlunit.html.HtmlFormUtil.submit;
 import static org.junit.Assert.*;
 
 /**
